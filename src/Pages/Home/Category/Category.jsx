8be89b5b32da4ab3b2e9edf971b69ec2d942dfbe
tanimtorikul <torikulswe@gmail.com>
2023-11-13 11:18:57 +0600

    added a component for section title and implemented food category

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -7,51 +7,68 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/sectionTitle/sectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      autoplay={{ delay: 1000 }} // 
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <div className="swiper-image-container">
-          <img src={slide1} alt="" />
-          <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">Salads</h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="swiper-image-container">
-          <img src={slide2} alt="" />
-          <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">Pizzas</h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="swiper-image-container">
-          <img src={slide3} alt="" />
-          <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">Soups</h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="swiper-image-container">
-          <img src={slide4} alt="" />
-          <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">Desserts</h3>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="swiper-image-container">
-          <img src={slide5} alt="" />
-          <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">Vegetables</h3>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    <section className="py-12">
+      <SectionTitle
+        subHeading={"From 11:00am to 10:00pm"}
+        heading={"Order Online"}
+      ></SectionTitle>
+      <Swiper
+        autoplay={{ delay: 1000 }} //
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="swiper-image-container">
+            <img src={slide1} alt="" />
+            <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">
+              Salads
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-image-container">
+            <img src={slide2} alt="" />
+            <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">
+              Pizzas
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-image-container">
+            <img src={slide3} alt="" />
+            <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">
+              Soups
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-image-container">
+            <img src={slide4} alt="" />
+            <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">
+              Desserts
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-image-container">
+            <img src={slide5} alt="" />
+            <h3 className="text-[32px] uppercase text-center -mt-12 text-white ">
+              Vegetables
+            </h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="flex flex-col items-center my-12 space-y-3">
+      <p className="text-[#D99904] text-[20px]">---{subHeading}---</p>
+      <p className="border border-[#e3e3e3] w-1/6"></p>
+      <p className="uppercase text-[40px] font-medium">{heading}</p>
+      <p className="border border-[#e3e3e3] w-1/6"></p>
+    </div>
+  );
+};
+
+export default SectionTitle;
