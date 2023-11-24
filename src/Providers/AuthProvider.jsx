96d8336d9b8d5ba51a18bb898b9f78dbbfb4a9f4
tanimtorikul <torikulswe@gmail.com>
2023-11-24 19:35:48 +0600

    create jwt token and saved on localstorage

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -18,6 +19,8 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const googleProvider = new GoogleAuthProvider();
+
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -46,8 +49,15 @@ const AuthProvider = ({ children }) => {
       setUser(currentUser);
       if (currentUser) {
         //  get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
       } else {
         // remove token (if token stored in the client side)
+        localStorage.removeItem("access-token");
       }
       setLoading(false);
     });
